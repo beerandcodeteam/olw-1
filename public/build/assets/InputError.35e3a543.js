b97ac3118ce715dfeb874dfbc2777da31ab52c65
Lucas Souza <lucas.roberto@creditas.com>
2024-02-20 01:53:41 +0000

    deploy builderd

---
+++ public/build/assets/InputError.35e3a543.js
@@ -0,0 +1,1 @@
+import{h as s,v as t,o as a,g as r,d as o,t as c}from"./app.a9da22e9.js";const n={class:"text-sm text-red-600"},p={__name:"InputError",props:["message"],setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{p as _};
